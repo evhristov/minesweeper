5f8b7ac1be0a1cbccadc109e8e19eb32ba904dac
evhristov <93148556+evhristov@users.noreply.github.com>
2021-10-25 22:06:51 +0300

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,6 @@
 from random import randint
 from collections import deque
 from string import ascii_uppercase
-import os
-
 
 
 def generate_field(size, mines):
@@ -25,19 +23,22 @@ def generate_field(size, mines):
                         if not field[x+i][y+j] == "*":
                             field[x+i][y+j] += 1
 
-    for _ in field:
-        _ = [str(x) for x in _]
-        print(" ".join(_))
+    # for _ in field:
+    #     _ = [str(x) for x in _]
+    #     print(" ".join(_))
 
     return field
 
 
 def make_selection():
     alph = list(ascii_uppercase)
+    command = input().split()
+    if command[0] == "MM":
+        mark_mine(int(command[2])-1,alph.index(command[1]))
+        return False
     try:
-        y_input, x_input = input().split()
-        y_input = alph.index(y_input)
-        x_input = int(x_input)-1
+        y_input = alph.index(command[0])
+        x_input = int(command[1])-1
     except:
         print("Please input 'A 1' format!")
         return
@@ -73,6 +74,16 @@ def show_all_zero(x_input,y_input):
                             zero_stack.append((x + i, y + j))
 
 
+def mark_mine(x,y):
+    if player_field[x][y] == " ":
+        player_field[x][y] = "M"
+    elif player_field[x][y] == "M":
+        player_field[x][y] = " "
+    else:
+        print("Cannot mark mine there!")
+    return
+
+
 
 
 
@@ -87,30 +98,23 @@ def show_field(player_field):
             result = " "
         else:
             result = ""
-        result += f"{row + 1}| " + " | ".join(map(str,player_field[row])) + " | "
+        result += f"{row + 1}| "+" | ".join(map(str,player_field[row]))+" | "
         print(result)
-        print("--|"+"---|"*(size))
+        print("--|"+"---|"*size)
         row += 1
 
 
-
 if __name__ == "__main__":
-
     while True:
         ded = False
         size = int(input("Please input number of rows:"))
         mines = int(input("Please input number of mines:"))
         field = generate_field(size, mines)
-        player_field  = [[" "]*size for _ in range(size)]
+        player_field = [[" "]*size for _ in range(size)]
 
         while not ded:
             show_field(player_field)
             ded = make_selection()
 
-
-
         if input("U ded!\nWould you like to play again? (Y/N):") not in ["Y","y"]:
             break
-
-
-
